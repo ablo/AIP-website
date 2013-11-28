8f91f2365226d9a2635bef33867d6e425904c34a
ablo <johan@johanabelson.com>
2013-11-28 11:40:26 +0100

    Updated file structure and added gitignore files.

---
+++ timthumb-config.php
@@ -0,0 +1,16 @@
+<?
+
+	$ALLOWED_SITES = array (
+		'flickr.com',
+		'staticflickr.com',
+		'facebook.com',
+		'picasa.com',
+		'img.youtube.com',
+		'upload.wikimedia.org',
+		'photobucket.com',
+		'imgur.com',
+		'imageshack.us',
+		'tinypic.com',
+	);
+
+?>
